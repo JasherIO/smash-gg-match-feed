8c6a3324f623fad37d5fc6682115287edb38b664
Willis <willis2342@gmail.com>
2017-10-01 06:03:30 -0500

    JSON

--- scraper.py
+++ scraper.py
@@ -1,17 +1,46 @@
-#!/usr/bin/env python
+# Author: Joshua C. Willis
+# License: MIT
+# Copyright: 2017
 
 import requests
 import json
 
-# url = 'https://smash.gg/tournament/nexus-gaming-saturday-150-na-3v3-weekly-tournament-36/details'
+import parser
 
-base = 'https://api.smash.gg/tournament/nexus-gaming-saturday-150-na-3v3-weekly-tournament-36'
-expands = '?expand[]=groups'
-url = base + expands
+def phases(slug):
+  base = 'https://api.smash.gg/tournament/'
+  expands = '?expand[]=phase&expand[]=groups'
+  url = base + slug + expands
 
-r = requests.get(url)
-tournament = json.loads(r.text)
-# print json.dumps(tournament, indent=2, sort_keys=True)
+  r = requests.get(url)
+  tournament = json.loads(r.text)
 
-groups = map(lambda x: x['id'], tournament['entities']['groups'])
-print groups
+  phases = {}
+  parsed = parser.phases(tournament['entities']['phase'])
+  for id, name in parsed.items():
+    # Group IDs for this phase
+    groupIds = [group['id'] for group in tournament['entities']['groups'] if group['phaseId'] == id]
+    for groupId in groupIds:
+      if name in phases:
+        phases[name][groupId] = {}
+      else:
+        phases[name] = { groupId: {} }
+
+  return phases
+
+
+def sets(groupId):
+
+  base = 'https://api.smash.gg/phase_group/'
+  expands = '?expand[]=entrants&expand[]=sets'
+
+  url = base + str(groupId) + expands
+  r = requests.get(url)
+
+  group = json.loads(r.text)
+
+  entrants = parser.entrants(group['entities']['entrants'])
+  return {
+    group['entities']['groups']['identifier']: parser.sets(group['entities']['sets'], entrants)
+  }
+  
